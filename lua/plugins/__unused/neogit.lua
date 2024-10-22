8df529cda04a5adea8403d404e8b1d5899b7afcc
Rafiq Arbee <rfqarbee@gmail.com>
2024-10-22 19:09:10 +0800

    replacing old file

--- lua/plugins/__unused/neogit.lua
+++ lua/plugins/__unused/neogit.lua
@@ -3,16 +3,55 @@ return {
   enabled = false,
   dependencies = {
     "nvim-lua/plenary.nvim", -- required
-    "sindrets/diffview.nvim", -- optional - Diff integration
-    "nvim-telescope/telescope.nvim",
+    {
+      "sindrets/diffview.nvim",
+      opts = function(_, opts)
+        local actions = require("diffview.actions")
+        opts.keymaps = {
+          view = {
+            ["<c-n>"] = actions.select_next_entry,
+            ["<c-p>"] = actions.select_prev_entry,
+          },
+          file_history_panel = {
+            ["<c-n>"] = actions.select_next_entry,
+            ["<c-p>"] = actions.select_prev_entry,
+          },
+          file_panel = {
+            ["<c-n>"] = actions.select_next_entry,
+            ["<c-p>"] = actions.select_prev_entry,
+          },
+        }
+      end,
+    },
   },
-  keys = {
-    { "<leader>nf", "<cmd>Neogit cwd=%:p:h<cr>", desc = "Git status (File Path)" },
-    { "<leader>ns", "<cmd>Neogit<cr>", desc = "Git Status (Root)" },
-    { "<leader>nd", "<cmd>Neogit diff<cr>", desc = "Neogit Diff" },
-  },
-  opts = {
-    disable_line_numbers = false,
-    disable_signs = true,
-  },
+  opts = function(_, opts)
+    opts.disable_line_numbers = false
+    opts.disable_insert_on_commit = true
+    opts.sections = {
+      recent = {
+        folded = false,
+      },
+    }
+    opts.status = {
+      recent_commit_count = 15,
+    }
+    opts.mappings = {
+      status = {
+        ["1"] = false,
+        ["2"] = false,
+        ["3"] = false,
+        ["4"] = false,
+      },
+    }
+    opts.signs = {
+      hunk = { "", "" },
+      section = { "", "󰅀" },
+      item = { "", "" },
+    }
+    -- vim.keymap.set("n", "<leader>ns", "<cmd>Neogit<cr>", { desc = "Git status" })
+    -- vim.keymap.set("n", "gs", "<cmd>Neogit<cr>", { desc = "Git status" })
+    -- vim.keymap.set("n", "<leader>gs", "<cmd>Neogit cwd=%:p:h<cr>", { desc = "Git current buffer path status" })
+    -- vim.keymap.set("n", "<leader>gd", "<cmd>Neogit diff<cr>", { desc = "Git diff" })
+    -- vim.keymap.set("n", "<leader>nc", "<cmd>Neogit cwd=%:p:h commit<cr>", { desc = "Git status" })
+  end,
 }
