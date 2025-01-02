7e51a555da8ffb6622b126bae6df61a9cd5e2ec4
Rafiq Arbee <rfqarbee@gmail.com>
2025-01-02 22:24:21 +0800

    test

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,16 +1,16 @@
 return {
   {
     "tpope/vim-fugitive",
-    enabled = true,
+    enabled = false,
     config = function()
       local map = require("custom.utils").map
       map("n", "gs", "<cmd>tab G<cr>", { silent = true, desc = "Git Fugitive" })
       map("n", "gS", "<cmd>G status<cr>", { silent = true, desc = "Git status cli" })
       map("n", "<leader>gm", "<cmd>Gvdiffsplit!<cr>", { desc = "Git merge 3 way" })
       -- map("n", "<leader>gd", "<cmd>Gvdiffsplit<cr>", { desc = "Diff this" })
-
       map("n", "gh", "<cmd>diffget //2<cr>", { desc = "changes from left" })
       map("n", "gl", "<cmd>diffget //3<cr>", { desc = "change from rigth" })
+      map("n", "<leader>gl", "<cmd>G log -n 50 --oneline<cr>")
     end,
   },
   {
@@ -28,8 +28,16 @@ return {
           fzf_lua = true,
         },
       })
-      require("custom.utils").map("n", "<leader>gs", "<cmd>Neogit cwd=%:p:h<cr>")
+      require("custom.utils").map("n", "gs", "<cmd>Neogit cwd=%:p:h<cr>")
       require("custom.utils").map("n", "<leader>gd", "<cmd>DiffviewOpen<cr>")
+
+      vim.api.nvim_create_autocmd("User", {
+        group = vim.api.nvim_create_augroup("NeogitEventTest", { clear = true }),
+        pattern = "NeogitCommitComplete",
+        callback = function()
+          print("hello from neogit")
+        end,
+      })
     end,
   },
 }
