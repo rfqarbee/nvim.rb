ff7f93ba1584f6799e98e8628de51ff185f70ca1
Rafiq Arbee <rfqarbee@gmail.com>
2025-06-01 01:38:48 +0800

    change go event attach

--- lua/plugins/lsp/go.lua
+++ lua/plugins/lsp/go.lua
@@ -1,7 +1,6 @@
 -- experimenting
 return {
   "ray-x/go.nvim",
-  event = { "CmdlineEnter" },
   ft = { "go", 'gomod' },
   build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries,
   dependencies = {                                        -- optional packages
