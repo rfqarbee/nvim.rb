4f54fa7fd63cf85ca53ca7255f4492361207cef8
Rafiq Arbee <rfqarbee@gmail.com>
2025-06-21 21:59:54 +0800

    try

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -1,13 +1,15 @@
 return {
-  'rmagatti/auto-session',
-  lazy = false,
-  keys = {
-    { '<leader>wr', '<cmd>SessionSearch<CR>',      desc = 'Session search' },
-    { '<leader>ws', '<cmd>SessionSave<CR>',        desc = 'Save session' },
-    { '<leader>wd', '<cmd>Autosession delete<CR>', desc = 'Delete' },
-  },
-  opts = {
-    suppressed_dirs = { '~/', '~/repos', '~/Downloads', '/' },
-    git_use_branch_name = true,
+  {
+    'rmagatti/auto-session',
+    lazy = false,
+    keys = {
+      { '<leader>wr', '<cmd>SessionSearch<CR>',      desc = 'Session search' },
+      { '<leader>ws', '<cmd>SessionSave<CR>',        desc = 'Save session' },
+      { '<leader>wd', '<cmd>Autosession delete<CR>', desc = 'Delete' },
+    },
+    opts = {
+      suppressed_dirs = { '~/', '~/repos', '~/Downloads', '/' },
+      git_use_branch_name = true,
+    },
   }
 }
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,6 +2,21 @@ return {
   {
     'deparr/tairiki.nvim',
     lazy = false,
-    priority = 1000, -- recommended if you use tairiki as your default theme
+    priority = 1000,
+    opts = {
+      palette = "light",
+      code_style = {
+        comments = { italic = true },
+        keywords = { italic = true },
+        functions = { italic = true },
+        types = { italic = true }
+      },
+    }
   },
+  {
+    "nickkadutskyi/jb.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  }
 }
