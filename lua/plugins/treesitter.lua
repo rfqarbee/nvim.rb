9967e00f2424c8118255559712df8096dbb9298d
Rafiq Arbee <rfqarbee@gmail.com>
2025-08-31 21:28:04 +0800

    dropbar configs

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,41 @@
 return {
   {
-    'Bekaboo/dropbar.nvim',
+    "Bekaboo/dropbar.nvim",
     config = function()
-      local dropbar_api = require('dropbar.api')
-      vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
-      vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
-      vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
+      local dropbar_api = require("dropbar.api")
+      require("dropbar").setup({
+        bar = {
+          sources = function(buf, _)
+            local sources = require("dropbar.sources")
+            local utils = require("dropbar.utils")
+            if vim.bo[buf].ft == "markdown" then
+              return {
+                sources.path,
+                sources.markdown,
+              }
+            end
+            if vim.bo[buf].buftype == "terminal" then
+              return {
+                sources.terminal,
+              }
+            end
+            return {
+              utils.source.fallback({
+                sources.lsp,
+                sources.treesitter,
+              }),
+            }
+          end,
+        },
+      })
+      vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
+      vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
+      vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
     end,
   },
   {
     "nvim-treesitter/nvim-treesitter-textobjects",
-    dependencies = { "nvim-treesitter/nvim-treesitter", },
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
     event = { "BufReadPre", "BufNewFile" },
     build = ":TSUpdate",
     config = function()
@@ -58,9 +83,9 @@ return {
               ["ic"] = "@class.inner",
             },
             selection_modes = {
-              ['@parameter.outer'] = 'v', -- charwise
-              ['@function.outer'] = 'V',  -- linewise
-              ['@class.outer'] = '<c-v>', -- blockwise
+              ["@parameter.outer"] = "v", -- charwise
+              ["@function.outer"] = "V", -- linewise
+              ["@class.outer"] = "<c-v>", -- blockwise
             },
             include_surrounding_whitespace = true,
           },
